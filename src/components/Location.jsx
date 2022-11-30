e9ffa39715a6359d98268b31dc9657d7c58c89dd
unaldutlu <unaldutlu@gmail.com>
2022-12-01 01:57:06 +0300

    add location on page

---
+++ src/components/Location.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Location() {
+  return (
+    <div className="bg-slate-400 h-16 flex items-center rounded-xl">
+      <div className="pl-6">Location</div>
+    </div>
+  );
+}
+
+export default Location;
