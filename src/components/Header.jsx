ac38cda032a773c15cbe66dd9a67e0901e4f569d
unaldutlu <unaldutlu@gmail.com>
2022-12-01 01:56:53 +0300

    add header update on page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import WeatherContext from "../context/WeatherContext";
 
 function Header() {
   const data = useContext(WeatherContext);
+  console.log(data);
 
   return (
     <div>
-      {/* <h1>{data}</h1> */}
-      <h1 className="uppercase font-extrabold text-3xl text-red-700 text-center pt-[12%]">weather forecast</h1>
+      <h1 className='uppercase font-extrabold text-xl sm:text-2xl lg:text-3xl text-red-700 text-center pt-[12%]'>
+        weather forecast
+      </h1>
     </div>
   );
 }
