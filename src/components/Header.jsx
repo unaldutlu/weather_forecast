7216e0764d2927c058276b9b3a46925af50c8347
unaldutlu <unaldutlu@gmail.com>
2022-12-01 00:21:05 +0300

    add header folder on page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import { useContext } from "react";
-import weatherContext from "../context/WeatherContext";
+import WeatherContext from "../context/WeatherContext";
+
 function Header() {
-  const data = useContext(weatherContext);
-  console.log(data);
-  return <div>Header</div>;
+  const data = useContext(WeatherContext);
+
+  return (
+    <div>
+      {/* <h1>{data}</h1> */}
+      <h1 className="uppercase font-extrabold text-3xl text-red-700 text-center pt-[12%]">weather forecast</h1>
+    </div>
+  );
 }
 
 export default Header;
