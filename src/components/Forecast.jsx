3ac8cf1fe90f685d4588d9897cbb071b13990d7b
unaldutlu <unaldutlu@gmail.com>
2022-12-03 23:55:33 +0300

    unnecessary codes remove on page

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,38 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import WeatherContext from "../context/WeatherContext";
 
 function Forecast() {
   const { weather, location } = useContext(WeatherContext);
-  const [dayy, setDayy] = useState([]);
-
-  // let daysInWeek = [
-  //   "Sunday",
-  //   "Monday",
-  //   "Tuesday",
-  //   "Wednesday",
-  //   "Thursday",
-  //   "Friday",
-  //   "Saturday",
-  // ];
-  // let days = [];
-
-  // for (let item of weather) {
-  //   let date = new Date(item.dt_txt);
-  //   date = date.getDay();
-  //   days.push(daysInWeek[date]);
-  //   console.log(date);
-  // }
-  // useEffect(
-  //   (day) => {
-  //     const dt = weather?.list[0].dt;
-  //     const date = new Date(dt * 1000);
-  //     const day = date.toUTCString().split(",")[0];
-  //     console.log(day);
-  //     setDayy(day);
-  //   },
-  //   [location]
-  // );
-  console.log(weather);
 
   return (
     <div className='bg-slate-200 p-4 m-4 mt-0 flex flex-wrap justify-evenly items-center rounded-xl gap-3'>
@@ -54,72 +24,6 @@ function Forecast() {
             </div>
           </div>
         ))}
-      {/* <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-          Tuesday
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-          Wednesday
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-          Thursday
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-          Friday
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-          Saturday
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-          Sunday
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div> */}
     </div>
   );
 }
