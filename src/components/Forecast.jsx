52a99562dd8180af9e13199ac60ad8cf07199f85
unaldutlu <unaldutlu@gmail.com>
2022-12-03 23:53:57 +0300

    add update on page

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,11 +1,62 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import WeatherContext from "../context/WeatherContext";
 
 function Forecast() {
+  const { weather, location } = useContext(WeatherContext);
+  const [dayy, setDayy] = useState([]);
+
+  // let daysInWeek = [
+  //   "Sunday",
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday",
+  // ];
+  // let days = [];
+
+  // for (let item of weather) {
+  //   let date = new Date(item.dt_txt);
+  //   date = date.getDay();
+  //   days.push(daysInWeek[date]);
+  //   console.log(date);
+  // }
+  // useEffect(
+  //   (day) => {
+  //     const dt = weather?.list[0].dt;
+  //     const date = new Date(dt * 1000);
+  //     const day = date.toUTCString().split(",")[0];
+  //     console.log(day);
+  //     setDayy(day);
+  //   },
+  //   [location]
+  // );
+  console.log(weather);
+
   return (
     <div className='bg-slate-200 p-4 m-4 mt-0 flex flex-wrap justify-evenly items-center rounded-xl gap-3'>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center font-semibold'>
-        Monday
+      {weather && console.log(weather.list)}
+      {weather &&
+        weather.list.map((el, i) => (
+          <div
+            key={i}
+            className='border-2 border-solid border-slate-600 w-28 h-36'
+          >
+            <div className='bg-red-300 h-9 flex justify-center items-center font-semibold'>
+              {new Date(el.dt * 1000).toUTCString().split(",")[0]}
+            </div>
+            <div className='bg-red-500 h-20 flex justify-center items-center'>
+              İcon
+            </div>
+            <div className='bg-red-700 flex justify-center items-center'>
+              Derece
+            </div>
+          </div>
+        ))}
+      {/* <div className='border-2 border-solid border-slate-600 w-28 h-36'>
+        <div className='bg-red-300 h-9 flex justify-center items-center'>
+          Tuesday
         </div>
         <div className='bg-red-500 h-20 flex justify-center items-center'>
           İcon
@@ -16,7 +67,7 @@ function Forecast() {
       </div>
       <div className='border-2 border-solid border-slate-600 w-28 h-36'>
         <div className='bg-red-300 h-9 flex justify-center items-center'>
-        Tuesday 
+          Wednesday
         </div>
         <div className='bg-red-500 h-20 flex justify-center items-center'>
           İcon
@@ -27,7 +78,7 @@ function Forecast() {
       </div>
       <div className='border-2 border-solid border-slate-600 w-28 h-36'>
         <div className='bg-red-300 h-9 flex justify-center items-center'>
-        Wednesday 
+          Thursday
         </div>
         <div className='bg-red-500 h-20 flex justify-center items-center'>
           İcon
@@ -38,7 +89,7 @@ function Forecast() {
       </div>
       <div className='border-2 border-solid border-slate-600 w-28 h-36'>
         <div className='bg-red-300 h-9 flex justify-center items-center'>
-        Thursday 
+          Friday
         </div>
         <div className='bg-red-500 h-20 flex justify-center items-center'>
           İcon
@@ -49,7 +100,7 @@ function Forecast() {
       </div>
       <div className='border-2 border-solid border-slate-600 w-28 h-36'>
         <div className='bg-red-300 h-9 flex justify-center items-center'>
-        Friday 
+          Saturday
         </div>
         <div className='bg-red-500 h-20 flex justify-center items-center'>
           İcon
@@ -60,7 +111,7 @@ function Forecast() {
       </div>
       <div className='border-2 border-solid border-slate-600 w-28 h-36'>
         <div className='bg-red-300 h-9 flex justify-center items-center'>
-        Saturday 
+          Sunday
         </div>
         <div className='bg-red-500 h-20 flex justify-center items-center'>
           İcon
@@ -68,18 +119,7 @@ function Forecast() {
         <div className='bg-red-700 flex justify-center items-center'>
           Derece
         </div>
-      </div>
-      <div className='border-2 border-solid border-slate-600 w-28 h-36'>
-        <div className='bg-red-300 h-9 flex justify-center items-center'>
-        Sunday 
-        </div>
-        <div className='bg-red-500 h-20 flex justify-center items-center'>
-          İcon
-        </div>
-        <div className='bg-red-700 flex justify-center items-center'>
-          Derece
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 }
