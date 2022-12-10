5e37ecfe27506d37fe71797f814e1b51f51f12ae
unaldutlu <unaldutlu@gmail.com>
2022-12-10 23:05:18 +0300

    add background on page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import Location from "./Location";
 
 function Card() {
   return (
-    <div className='border-2 border-solid border-slate-600 rounded-xl w-auto bg-slate-400 m-10 mt-3'>
+    <div className='border-2 border-solid border-slate-600 rounded-xl w-auto bg-slate-400 m-10 mt-3 opacity-80 panel'>
       <Location />
       <Forecast />
     </div>
